c8ae85422fb5ebdc96dd1268fe2eb4d737a43e67
Jim Fleming <jimmy.fleming@gmail.com>
2011-08-27 08:34:34 -0400

    adding server.js for linode and deploy scripts

---
+++ server.js
@@ -0,0 +1,71 @@
+(function() {
+  var PlayerModel, PlayersCollection, Vector, app, express, game_loop, io, players, _;
+  express = require('express');
+  _ = require('underscore');
+  Vector = require('./src/scripts/vector');
+  PlayerModel = require('./src/scripts/players/player.model');
+  PlayersCollection = require('./src/scripts/players/players.collection');
+  app = express.createServer();
+  app.use(express.compiler({
+    src: "" + __dirname + "/src",
+    dest: "" + __dirname + "/public",
+    enable: ['coffeescript', 'less']
+  }));
+  app.use(express.static("" + __dirname + "/public"));
+  app.listen(80);
+  console.log("listening on " + 80 + "...");
+  io = require('socket.io').listen(app);
+  io.configure('production', function() {
+    io.enable('browser client minification');
+    io.enable('browser client etag');
+    io.set('log level', 1);
+    return io.set('transports', ['websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
+  });
+  io.configure('development', function() {
+    return io.set('transports', ['websocket']);
+  });
+  players = new PlayersCollection();
+  players.bind('remove', function(player) {
+    return io.sockets.volatile.emit('player:disconnect', player.toJSON());
+  });
+  game_loop = function() {
+    players.update();
+    io.sockets.volatile.emit('players:update', players.toJSON());
+    return setTimeout(function() {
+      return game_loop();
+    }, 1000 / 60);
+  };
+  game_loop();
+  io.sockets.on('connection', function(socket) {
+    var player, team;
+    console.log("player connected: " + socket.id);
+    team = players.spores().length > players.ships().length ? 'ships' : 'spores';
+    player = new PlayerModel({
+      id: socket.id,
+      team: team
+    });
+    player.players = players;
+    players.add(player, {
+      silent: true
+    });
+    socket.on('player:update', function(action, callback) {
+      var player_state;
+      player_state = player.get('state');
+      switch (action) {
+        case 'LEFT':
+          return player.move_right();
+        case 'RIGHT':
+          return player.move_left();
+        case 'DOWN':
+          return player.aim_left();
+        case 'UP':
+          return player.aim_right();
+      }
+    });
+    return socket.on('disconnect', function() {
+      console.log("player disconnected: " + socket.id);
+      player = players.get(socket.id);
+      return players.remove(player);
+    });
+  });
+}).call(this);
